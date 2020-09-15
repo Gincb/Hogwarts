fe4e9842036e6c4edb6b6770855a9251e1db8a49
Gintare <gintaar3@gmail.com>
2020-09-15 14:47:07 +0200

    modal and changes

--- script.js
+++ script.js
@@ -2,7 +2,25 @@
 
 window.addEventListener("DOMContentLoaded", getData);
 
+//Add event to hide modal
+const modalClose = document.querySelector(".close");
+const modal = document.querySelector(".modal-background");
+
+modalClose.addEventListener("click", () => {
+  modal.classList.add("hide");
+});
+
 let studentObject = [];
+
+let oneStudent = {
+  firstname: "",
+  middlename: "",
+  lastname: "",
+  nickname: "",
+  gender: "",
+  house: "",
+  image: "",
+};
 
 function getData() {
   fetch("https://petlatkea.dk/2020/hogwarts/students.json")
@@ -11,77 +29,27 @@ function getData() {
 }
 
 function handleData(students) {
-  students.forEach(showData);
+  students.forEach(prepareData);
 }
 
-function showData(data) {
-  //Put data to the template
-  const templateElement = document.querySelector("#template").content;
-  const myClone = templateElement.cloneNode(true);
+function prepareData(data) {
+  const student = Object.create(oneStudent);
+  console.table(data);
+  nameSeperator(data, student);
 
-  data.fullname.trim(); //Delete white space
-  const seperateName = data.fullname.trim().split(" "); //Seperate names into parts
-
-  studentObject.firstname =
-    seperateName[0][0].toUpperCase() +
-    seperateName[0].substring(1).toLowerCase(); //Get first name
-
-  studentObject.lastname = seperateName.pop(); //Get last name
-  studentObject.lastname =
-    studentObject.lastname[0].toUpperCase() +
-    studentObject.lastname.substring(1).toLowerCase(); //Get last name
-
-  if (studentObject.lastname.includes("-")) {
-    studentObject.lastname =
-      studentObject.lastname.substring(
-        0,
-        studentObject.lastname.indexOf("-") + 1
-      ) +
-      studentObject.lastname[
-        studentObject.lastname.indexOf("-") + 1
-      ].toUpperCase() +
-      studentObject.lastname.substring(studentObject.lastname.indexOf("-") + 2);
-  } //Uppercase names with hyphen
-
-  if (seperateName[1] != studentObject.lastname) {
-    studentObject.middlename = seperateName[1]; //Get middle name
-  }
-
-  if (studentObject.middlename == undefined) {
-    studentObject.nickname = undefined; //Set nickname undefined comparing to middle name
-  } else if (studentObject.middlename.includes(`"`)) {
-    studentObject.nickname = studentObject.middlename.slice(1, -1); //Set nickname
-  }
-
-  if (studentObject.middlename == undefined) {
-    myClone.querySelector(".middle-name").classList.add("hide"); // Hide if there is no middlename
-  } else {
-    studentObject.middlename =
-      studentObject.middlename[0].toUpperCase() +
-      studentObject.middlename.substring(1);
-  }
-
-  if (studentObject.nickname == undefined) {
-    myClone.querySelector(".nick-name").classList.add("hide"); // Hide if there is no nickname
-  } else if (studentObject.nickname == studentObject.middlename.slice(1, -1)) {
-    studentObject.middlename = undefined;
-    myClone.querySelector(".middle-name").classList.add("hide"); // Check if nickname is same as middle name then hide
-  }
-
-  studentObject.house = data.house.trim();
-  studentObject.house =
-    studentObject.house[0].toUpperCase() +
-    studentObject.house.substring(1).toLowerCase(); //Capitalize house name
+  student.house = data.house.trim();
+  student.house =
+    student.house[0].toUpperCase() + student.house.substring(1).toLowerCase(); //Capitalize house name
 
   if (
-    studentObject.lastname.includes(`-`) ||
-    studentObject.firstname.includes(`-`)
+    (student.lastname && student.lastname.includes(`-`)) ||
+    student.firstname.includes(`-`)
   ) {
-    studentObject.image = `images/${studentObject.lastname
-      .substring(studentObject.lastname.indexOf("-") + 1)
-      .toLowerCase()}_${studentObject.firstname[0].toLowerCase()}.png`;
-  } else {
-    studentObject.image = `images/${studentObject.lastname.toLowerCase()}_${studentObject.firstname[0].toLowerCase()}.png`;
+    student.image = `images/${student.lastname
+      .substring(student.lastname.indexOf("-") + 1)
+      .toLowerCase()}_${student.firstname[0].toLowerCase()}.png`;
+  } else if (student.lastname) {
+    student.image = `images/${student.lastname.toLowerCase()}_${student.firstname[0].toLowerCase()}.png`;
   }
 
   //Check if image exists
@@ -92,29 +60,109 @@ function showData(data) {
     return xhr.status != 404;
   };
 
-  var url = studentObject.image;
+  var url = student.image;
 
   if (iamgeExists(url)) {
     console.log("This is fine");
   } else if (iamgeExists(url) == false) {
-    studentObject.image = `images/${studentObject.lastname.toLowerCase()}_${studentObject.firstname.toLowerCase()}.png`;
+    student.image = `images/${student.lastname.toLowerCase()}_${student.firstname.toLowerCase()}.png`;
   }
 
-  // if (iamgeExists(url) == false) {
-  //   studentObject.image = undefined;
-  //   myClone.querySelector(".img").classList.add("hide");
-  // }
+  studentObject.push(student);
 
-  myClone.querySelector(".first-name").innerHTML = studentObject.firstname;
-  myClone.querySelector(".middle-name").innerHTML = studentObject.middlename;
-  myClone.querySelector(".last-name").innerHTML = studentObject.lastname;
-  myClone.querySelector(".nick-name").innerHTML = studentObject.nickname;
-  myClone.querySelector(".gender").innerHTML = data.gender;
-  myClone.querySelector(".house").innerHTML = studentObject.house;
+  displayStudentList();
+}
 
-  myClone.querySelector(".img").src = studentObject.image;
+function displayStudentList() {
+  document.querySelector(".content").innerHTML = "";
+
+  studentObject.forEach(displayStudent);
+}
+
+function displayStudent(student) {
+  //Put data to the template
+  const templateElement = document.querySelector("#template").content;
+  const myClone = templateElement.cloneNode(true);
+
+  myClone.querySelector(".first-name").innerHTML = student.firstname;
+
+  myClone.querySelector(".img").src = student.image;
+  myClone.querySelector(".student").addEventListener("click", openModal);
+
+  function openModal() {
+    showModalContent(student);
+    modal.classList.remove("hide");
+  }
 
   const where = document.querySelector(".content");
   where.appendChild(myClone);
-  console.table(studentObject);
+}
+
+function nameSeperator(data, student) {
+  data.fullname.trim(); //Delete white space
+  const seperateName = data.fullname.trim().split(" "); //Seperate names into parts
+
+  student.firstname =
+    seperateName[0][0].toUpperCase() +
+    seperateName[0].substring(1).toLowerCase(); //Get first name
+
+  student.lastname = seperateName.pop(); //Get last name
+
+  if (student.lastname == student.firstname) {
+    student.lastname = undefined;
+  } else {
+    student.lastname =
+      student.lastname[0].toUpperCase() +
+      student.lastname.substring(1).toLowerCase(); //Get last name
+  }
+
+  if (student.lastname && student.lastname.includes("-")) {
+    student.lastname =
+      student.lastname.substring(0, student.lastname.indexOf("-") + 1) +
+      student.lastname[student.lastname.indexOf("-") + 1].toUpperCase() +
+      student.lastname.substring(student.lastname.indexOf("-") + 2);
+  } //Uppercase names with hyphen
+
+  if (seperateName[1] != student.lastname) {
+    student.middlename = seperateName[1]; //Get middle name
+  }
+
+  if (student.middlename == undefined) {
+    student.nickname = undefined; //Set nickname undefined comparing to middle name
+  } else if (student.middlename.includes(`"`)) {
+    student.nickname = student.middlename.slice(1, -1); //Set nickname
+  }
+
+  if (student.middlename) {
+    student.middlename =
+      student.middlename[0].toUpperCase() + student.middlename.substring(1);
+  }
+
+  if (student.nickname == undefined) {
+  } else if (student.nickname == student.middlename.slice(1, -1)) {
+    student.middlename = undefined;
+  }
+}
+
+function showModalContent(student) {
+  modal.querySelector(".modal-first-name").innerHTML = student.firstname;
+  modal.querySelector(".middle-name").innerHTML = student.middlename;
+
+  if (student.middlename == undefined) {
+    modal.querySelector(".middle-name").classList.add("hide"); // Hide if there is no middlename
+  }
+
+  if (student.nickname) {
+    modal.querySelector(".nick-name").innerHTML = student.nickname;
+    modal.querySelector(".nick-name-hide").classList.remove("hide"); // Hide if there is no middlename
+  } else {
+    modal.querySelector(".nick-name-hide").classList.add("hide");
+  }
+
+  modal.querySelector(".house-name").innerHTML = student.house;
+  modal.querySelector(".last-name").innerHTML = student.lastname;
+  modal.querySelector(".nick-name").innerHTML = student.nickname;
+  document.querySelector(".student-img").src = student.image;
+
+  modal.dataset.theme = student.house;
 }
